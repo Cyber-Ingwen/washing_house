45504e1990448781ef6e51e7f6785f288aeccfaa
User7680251 <2591592360@qq.com>
2022-07-28 09:02:16 +0000

    1

--- src/bynav/bynav/point_cloud.py
+++ src/bynav/bynav/point_cloud.py
@@ -37,52 +37,24 @@ class Node_PC(Node):
         self.vis.poll_events()
         self.vis.update_renderer()
 
-    def read_points(self, cloud, field_names=None, skip_nans=False, uvs=[]):
+    def read_points(self, cloud):
+        """读取点云数据"""
         assert isinstance(cloud, PointCloud2)
-        fmt = self._get_struct_fmt(cloud.is_bigendian, cloud.fields, field_names)
+        fmt = self._get_struct_fmt(cloud.is_bigendian, cloud.fields)
         width, height, point_step, row_step, data, isnan = cloud.width, cloud.height, cloud.point_step, cloud.row_step, cloud.data, math.isnan
         unpack_from = struct.Struct(fmt).unpack_from
 
-        if skip_nans:
-            if uvs:
-                for u, v in uvs:
-                    p = unpack_from(data, (row_step * v) + (point_step * u))
-                    has_nan = False
-                    for pv in p:
-                        if isnan(pv):
-                            has_nan = True
-                            break
-                    if not has_nan:
-                        yield p
-            else:
-                for v in range(height):
-                    offset = row_step * v
-                    for u in range(width):
-                        p = unpack_from(data, offset)
-                        has_nan = False
-                        for pv in p:
-                            if isnan(pv):
-                                has_nan = True
-                                break
-                        if not has_nan:
-                            yield p
-                        offset += point_step
-        else:
-            if uvs:
-                for u, v in uvs:
-                    yield unpack_from(data, (row_step * v) + (point_step * u))
-            else:
-                for v in range(height):
-                    offset = row_step * v
-                    for u in range(width):
-                        yield unpack_from(data, offset)
-                        offset += point_step
+        for v in range(height):
+            offset = row_step * v
+            for u in range(width):
+                yield unpack_from(data, offset)
+                offset += point_step
 
     def _get_struct_fmt(self, is_bigendian, fields, field_names=None):
         fmt = '>' if is_bigendian else '<'
 
         offset = 0
-        for field in (f for f in sorted(fields, key=lambda f: f.offset) if field_names is None or f.name in field_names):
+        for field in (f for f in sorted(fields, key=lambda f: f.offset)):
             if offset < field.offset:
                 fmt += 'x' * (field.offset - offset)
                 offset = field.offset
