442aae33d8cb7c17447f4db1574d31b0e6de3ccc
User7980251 <2591592360@qq.com>
2022-08-04 08:17:08 +0000

    😂

--- src/loam/src/point_cloud_node.cpp
+++ src/loam/src/point_cloud_node.cpp
@@ -2,26 +2,45 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/visualization/pcl_visualizer.h>
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
 
 class point_cloud_node: public rclcpp::Node
 {
-    /* 创建point_cloud节点 */
-    private:
-        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_point_cloud;
-        std::string topic_name = "/kitti/velo/pointcloud";
-
+    /*
+    创建point_cloud节点 
+    */
     public:
+        std::string viz_name;
+        boost::shared_ptr<pcl::visualization::PCLVisualizer> visualizer;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_point_cloud;
+
         point_cloud_node(std::string name): Node(name)
         {
             RCLCPP_INFO(this->get_logger(), "point_cloud_node已创建");
+
+            /*创建并初始化接收*/
+            std::string topic_name = "/kitti/velo/pointcloud";
             sub_point_cloud = this->create_subscription<sensor_msgs::msg::PointCloud2>(topic_name, 10, std::bind(&point_cloud_node::callback, this, std::placeholders::_1));
+        
+            /*配置可视化*/
+            viz_name = "pcl cloud";
+            visualizer = boost::make_shared<pcl::visualization::PCLVisualizer>(viz_name);
         }
 
         void callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg_ptr)
         {
-           pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZI>>();
+            /*处理点云*/
+            pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZI>>();
+            pcl::fromROSMsg(*msg_ptr, *cloud);
+
+            /*可视化点云*/
+            pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity(cloud, "intensity");
+            visualizer->removeAllPointClouds();
+            visualizer->addPointCloud(cloud, intensity, viz_name, 0);
+            visualizer->spinOnce(0.001);
+            RCLCPP_INFO(this->get_logger(), "succuse!");
         }
 };
 
