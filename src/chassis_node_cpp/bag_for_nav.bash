448c1c8b00d71fa5eb19cd3b57ee54aa0f3a49b2
tt <1113662441@qq.com>
2022-09-06 11:29:03 +0800

    12

--- src/chassis_node_cpp/bag_for_nav.bash
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt install ros-${ROS_DISTRO}-nav2-* \
-    ros-${ROS_DISTRO}-joint-state-publisher-gui \
-    ros-${ROS_DISTRO}-robot-localization \
-    ros-${ROS_DISTRO}-gazebo-ros-pkgs \
-    ros-${ROS_DISTRO}-ros2-control \
-    ros-${ROS_DISTRO}-xacro
-
